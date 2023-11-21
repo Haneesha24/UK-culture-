bb7134d9620b445af47cf3c42fcabc59d988d026
Haneesha24 <151597189+Haneesha24@users.noreply.github.com>
2023-11-21 11:26:54 +0530

    Add files via upload

---
+++ index.js.js
@@ -0,0 +1,23 @@
+﻿function handleSubmit () {
+    const name = document.getElementById('name').value;
+    const surname = document.getElementById('surname').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+
+
+    // to set into local storage
+    /* localStorage.setItem("NAME", name);
+    localStorage.setItem("SURNAME", surname);
+    localStorage.setItem("EMAIL", email);
+    localStorage.setItem("PASSWORD", password); */
+    
+    sessionStorage.setItem("NAME", name);
+    sessionStorage.setItem("SURNAME", surname);
+    sessionStorage.setItem("EMAIL", email);
+    sessionStorage.setItem("PASSWORD", password);
+
+
+
+    return;
+}
---
+++ result.js
@@ -0,0 +1,29 @@
+﻿﻿window.addEventListener('load', () => {
+
+    // Via Query parameters - GET
+    /* const params = (new URL(document.location)).searchParams;
+    const name = params.get('name');
+    const surname = params.get('surname');
+    const email = params.get('email');
+    const password = params.get('password'); */
+
+    // Via local Storage
+    /* const name = localStorage.getItem('NAME');
+    const surname = localStorage.getItem('SURNAME');
+    const email = localStorage.getItem('EMAIL');
+    const password = localStorage.getItem('PASSWORD'); */
+    
+    const name = sessionStorage.getItem('NAME');
+    const surname = sessionStorage.getItem('SURNAME');
+    const email = sessionStorage.getItem('EMAIL');
+    const password = sessionStorage.getItem('PASSWORD');
+
+    
+    document.getElementById('result-name').innerHTML = name;
+    document.getElementById('result-surname').innerHTML = surname;
+     document.getElementById('result-email').innerHTML = email;
+      document.getElementById('result-password').innerHTML = password;
+
+
+
+})
